a4029746fe1aea1bd55a9a5abf91126d373fcd06
LucasAmadio1 <lucasrochaamadio02@gmail.com>
2024-03-16 11:53:02 -0300

    iniciando o front do projeto

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Sujeito pizza</title>
+      </Head>
+
+      <div>
+        <h1>Hello world!</h1>
+      </div>
+    </>
+  )
+}
